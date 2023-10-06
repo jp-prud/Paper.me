bd3029e83d67aa0d0facaf6587825aa77d2c051c
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2023-10-06 16:18:11 -0300

    fix: change post service to hook structure

---
+++ src/services/PostService/PostService.ts
@@ -0,0 +1,40 @@
+import {CreatePostDTO, PostProps} from '@domain';
+
+import {HttpClient} from '../utils/HttpClient';
+
+export function PostService() {
+  async function listAll() {
+    const response = await HttpClient.get<PostProps[]>('posts');
+
+    if (response.status !== 200) {
+      throw new Error('Error on list posts');
+    }
+
+    return response.data;
+  }
+
+  async function resumeList() {
+    const response = await HttpClient.get<PostProps[]>('posts/resume');
+
+    return response.data;
+  }
+
+  async function findById(postId: PostProps['id']) {
+    const response = await HttpClient.get<PostProps>(`posts/${postId}`);
+
+    return response.data;
+  }
+
+  async function create(createPostDTO: CreatePostDTO) {
+    const response = await HttpClient.post<PostProps>('posts', createPostDTO);
+
+    return response.data;
+  }
+
+  return {
+    listAll,
+    resumeList,
+    findById,
+    create,
+  };
+}

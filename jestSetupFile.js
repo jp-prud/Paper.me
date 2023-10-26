c9cbd665ce1495257682231c6069c5e0d9337407
Joao Pedro Prudencio <joaopprudencio82@gmail.com>
2023-10-26 00:47:11 -0300

    test: define new async storage mock

--- jestSetupFile.js
+++ jestSetupFile.js
@@ -1,7 +1,4 @@
 import 'react-native-gesture-handler/jestSetup';
-import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
-
-jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 
 jest.mock('react-native-reanimated', () => {
   const Reanimated = require('react-native-reanimated/mock');

837a78174f8726a1bb08c825ab3debf9bb61ba5a
Joao Pedro Prudencio <joaopprudencio82@gmail.com>
2023-12-08 21:24:31 -0300

    feat: create initial profile screen implementation

--- src/screens/app/PostDetailsScreen/usePostDetailsScreen.tsx
+++ src/screens/app/PostDetailsScreen/usePostDetailsScreen.tsx
@@ -1,12 +1,25 @@
+import {useEffect} from 'react';
+
 import {PostProps} from '@types';
-import {useGetPostById} from '@useCases';
+import {useGetPostById, useSaveStoragePost} from '@useCases';
 
 export function usePostDetailsScreen(postId: PostProps['id']) {
   const {post, isLoading, isError} = useGetPostById(postId);
+
+  const {data, savePost} = useSaveStoragePost();
+
+  useEffect(() => {
+    if (isLoading || !post) {
+      return;
+    }
+
+    savePost(post);
+  }, [post, isLoading, savePost]);
 
   return {
     post,
     isLoading,
     isError,
+    data,
   };
 }
